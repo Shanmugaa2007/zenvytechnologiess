752f0590134491233b41181450a6a87b2cf3593c
Shanmuganathan <shanmuganathan232007@gmail.com>
2026-01-27 10:21:52 +0530

    Add files via upload

---
+++ src/MongoDB Schema/internship.mjs
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const InternshipSchema = new mongoose.Schema({
+    title:{
+        type: mongoose.Schema.Types.String,
+        required: true,
+    },
+    days:{
+        type: mongoose.Schema.Types.String,
+        required: true
+    },
+    price:{
+        type: mongoose.Schema.Types.Number,
+        required:true
+    },
+    image:{
+        type: mongoose.Schema.Types.String,
+        required: true
+    }
+})
+
+export const Internships = mongoose.model("Internship",InternshipSchema);
