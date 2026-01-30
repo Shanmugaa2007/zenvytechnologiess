c3fb32dcd9dc58247451c03fcba52bdf58f94be9
Shanmuganathan <shanmuganathan232007@gmail.com>
2026-01-30 17:32:35 +0530

    Refactor profile image schema and remove connection
    Removed database connection and added 'name' field to schema.

--- src/MongoDB Schema/profile.mjs
+++ src/MongoDB Schema/profile.mjs
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
-await mongoose.connect(
-  "mongodb+srv://zenvy:zenvy26@cluster0.krjlrpp.mongodb.net/zenvy"
-);
-
 const ProfileImageSchema = new mongoose.Schema({
-    image: {
+  name: {
     type: String,
-    required: true,
+  },
+  image: {
+    type: String,
   }
-})
+});
 
-export const profileImage = mongoose.model("UserProfileImage",ProfileImageSchema)
+export const profileImage = mongoose.model("UserProfileImage", ProfileImageSchema);
