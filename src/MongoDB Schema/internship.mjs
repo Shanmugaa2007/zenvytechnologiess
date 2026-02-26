23bfedf411225728d14f880f8cd9338016334211
Your Name <your-email@example.com>
2026-02-26 13:50:38 +0530

    change schema

--- src/MongoDB Schema/internship.mjs
+++ src/MongoDB Schema/internship.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from "mongoose";
 
 const InternshipSchema = new mongoose.Schema({
@@ -21,32 +20,3 @@ const InternshipSchema = new mongoose.Schema({
 })
 
 export const Internships = mongoose.model("Internships",InternshipSchema,"Internships");
-=======
-import mongoose from "mongoose";
-
-const InternshipSchema = new mongoose.Schema({
-    title:{
-        type: mongoose.Schema.Types.String,
-        required: true,
-    },
-    days:{
-        type: mongoose.Schema.Types.String,
-        required: true
-    },
-    price:{
-        type: mongoose.Schema.Types.Number,
-        required:true
-    },
-    image:{
-        type: mongoose.Schema.Types.String,
-        required: true
-    }
-})
-
-
-export const Internships = mongoose.model("Internships",InternshipSchema,"Internships");
-
-
-
-
->>>>>>> 9253b974459fd77046e9e3e6de162b3cab70711e
--- src/MongoDB Schema/profile.mjs
+++ src/MongoDB Schema/profile.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from "mongoose";
 
 const ProfileImageSchema = new mongoose.Schema({
@@ -11,17 +10,3 @@ const ProfileImageSchema = new mongoose.Schema({
 });
 
 export const profileImage = mongoose.model("UserProfileImage", ProfileImageSchema);
-=======
-import mongoose from "mongoose";
-
-const ProfileImageSchema = new mongoose.Schema({
-  name: {
-    type: String,
-  },
-  image: {
-    type: String,
-  }
-});
-
-export const profileImage = mongoose.model("UserProfileImage", ProfileImageSchema);
->>>>>>> 9253b974459fd77046e9e3e6de162b3cab70711e
