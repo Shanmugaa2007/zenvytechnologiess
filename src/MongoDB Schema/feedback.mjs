c84dbec8daa4711fd489f6dee116a7e17b742a16
Your Name <your-email@example.com>
2026-02-26 13:48:23 +0530

    second commit feedback mongo

--- src/MongoDB Schema/feedback.mjs
+++ src/MongoDB Schema/feedback.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from 'mongoose';
 
 await mongoose.connect(
@@ -20,27 +19,4 @@ const feedbackschema = new mongoose.Schema({
     }
 })
 
-=======
-import mongoose from 'mongoose';
-
-await mongoose.connect(
-  "mongodb+srv://zenvy:zenvy26@cluster0.krjlrpp.mongodb.net/zenvy"
-);
-
-const feedbackschema = new mongoose.Schema({
-    name:{
-        type: mongoose.Schema.Types.String,
-        required: true
-    },
-    rating:{
-        type: mongoose.Schema.Types.String,
-        required: true
-    },
-    message:{
-        type: mongoose.Schema.Types.String,
-        required: true
-    }
-})
-
->>>>>>> 9253b974459fd77046e9e3e6de162b3cab70711e
 export const Feedback = mongoose.model("feedback", feedbackschema, "feedbacks");
